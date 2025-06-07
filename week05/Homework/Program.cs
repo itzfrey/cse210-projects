af735a029bbebfd023210c4ecef91fe23270e226
itzfrey <hogan.godfrey1@gmail.com>
2025-06-08 00:40:56 +0100

    finished inheritance learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Dynamic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworklist());
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of Wold War II by Mary Waters");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
