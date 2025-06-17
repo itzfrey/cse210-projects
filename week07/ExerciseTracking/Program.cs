bed004b7bb03323476faaad6ff6d94e604ba8177
itzfrey <hogan.godfrey1@gmail.com>
2025-06-17 15:43:33 +0100

    finished exercise tracking program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Exercise> exerciseList = new List<Exercise>();
+        
+        Running running = new Running("June 15, 2025", 30, 4.8);
+        exerciseList.Add(running);
+
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("June 15, 2023", 60, 15);
+        exerciseList.Add(stationaryBicycles);
+
+        Swimming swimming = new Swimming("June 15, 2025", 20, 20);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList) 
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
